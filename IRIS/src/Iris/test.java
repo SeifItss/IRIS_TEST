b75688412e194d62ebf355a3007c6e7fec0c1e45
ITSS.LOCAL.LAN <ITSS.LOCAL.LAN@DESKTOP-UOE1NGA>
2020-09-14 17:00:18 +0100

    First

---
+++ IRIS/src/Iris/test.java
@@ -0,0 +1,13 @@
+package Iris;
+
+import com.temenos.t24.api.hook.system.RecordLifecycle;
+
+/**
+ * TODO: Document me!
+ *
+ * @author ITSS.LOCAL.LAN
+ *
+ */
+public class test extends RecordLifecycle {
+
+}
